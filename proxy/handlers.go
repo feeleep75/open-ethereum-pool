541f3e466a407b0244246f8ad9531d82910d7822
slavserver <onvosnovs@gmail.com>
2019-06-19 03:30:11 +0300

    Update handlers.go

--- proxy/handlers.go
+++ proxy/handlers.go
@@ -5,8 +5,8 @@ import (
 	"regexp"
 	"strings"
 
-	"github.com/feeleep75/open-ethereum-pool/rpc"
-	"github.com/feeleep75/open-ethereum-pool/util"
+	"github.com/slavserver/open-ethereum-pool/rpc"
+	"github.com/slavserver/open-ethereum-pool/util"
 )
 
 // Allow only lowercase hexadecimal with 0x prefix
